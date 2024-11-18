146e02c12c4b13741229a316111be70013b7a0ff
Clarkmsiv <dilarapoyraz753@gmail.com>
2024-11-18 02:18:31 +0100

    Add files via upload

---
+++ HomePage.js.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <button>Add New List</button>
+      {/* Burada listeleri göstereceğiz */}
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ ListDetailsPage.js.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListDetailsPage = () => {
+  return <div>Shopping List Details</div>;
+};
+
+export default ListDetailsPage;
